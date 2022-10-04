c8854e38393c4266720a9988509d4ce1b7086e70
Juan Huguet <jhuguetospina@gmail.com>
2022-10-03 21:36:43 -0500

    ADD: update in css styles

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,4 +1,5 @@
-import Products from "../utils/Products";
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.css";
 
 const Product = ({
   id,
@@ -8,21 +9,17 @@ const Product = ({
   description,
   imageURL,
 }) => {
-  console.log(`Products are: ${JSON.stringify(Products)}`);
   return (
-    <div>
-      <div>
-        <img src={imageURL} alt={name} />
-      </div>
-      <div>
-        <h1>hello</h1>
-        <h3>{name}</h3>
-        <h4>{description}</h4>
-        <h4>{available_amount}</h4>
-        <h4>$ {price}</h4>
-        <button>Add To Cart</button>
-      </div>
-    </div>
+    <Card style={{ width: "18rem", margin: "1rem" }}>
+      <Card.Img variant="top" src={imageURL}></Card.Img>
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>
+          {description}: ${price}
+        </Card.Text>
+      </Card.Body>
+      <button style={{ marginBottom: "1rem" }}>Add To Cart</button>
+    </Card>
   );
 };
 
--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,21 +1,25 @@
 import Products from "../utils/Products";
 import Product from "./Product";
+import Row from "react-bootstrap/Row";
 
 const ProductList = () => {
   return (
-    <div>
-      {Products.map((product) => {
-        return (
-          <Product
-            id={product.id}
-            name={product.name}
-            price={product.price}
-            available_amount={product.available_amount}
-            description={product.description}
-            imageURL={product.imageURL}
-          />
-        );
-      })}
+    <div className="col-6">
+      <Row xs={1} md={2}>
+        {Products.map((product, index) => {
+          return (
+            <Product
+              key={index}
+              id={product.id}
+              name={product.name}
+              price={product.price}
+              available_amount={product.available_amount}
+              description={product.description}
+              imageURL={product.imageURL}
+            />
+          );
+        })}
+      </Row>
     </div>
   );
 };
